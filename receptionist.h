6dd531a2147d2723ec01c45bba5e3ee52f91425d
Sumit Ranjan Satapathy <satapathysumit10@gmail.com>
2025-04-30 19:21:29 +0530

    initial commit

---
+++ receptionist.h
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include <iostream>
+#include "person.h"
+#include "appointment.h"
+#ifndef Doctor_H
+#define DOCTOR_H
+class Receptionist : public Person
+{
+private:
+    Appointment appointment;
+
+public:
+    Receptionist() : Person()
+    {
+    }
+    Receptionist(Appointment appointment, string firstName, string lastName, string gender, string dateOfBirth, string phoneNumber, string receptionistId) : Person(firstName, lastName, gender, dateOfBirth, phoneNumber, receptionistId)
+    {
+        this->appointment = appointment;
+    }
+    ~Receptionist()
+    {
+    }
+    Appointment getAppointment() const
+    {
+        return this->appointment;
+    }
+    string getReceptionistId() const
+    {
+        return this->getId();
+    }
+    void setAppointment(Appointment appointment)
+    {
+        this->appointment = appointment;
+    }
+    void printAppointmentInfo()
+    {
+        this->appointment.printAppointment();
+    }
+    void setReceptionistId(string receptionistId)
+    {
+        this->setId(receptionistId);
+    }
+};
+#endif
